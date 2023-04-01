6cc10237aa59cd8ce7cab0c65a4929b76108d46d
lawcows <129429387+lawcows@users.noreply.github.com>
2023-04-01 15:42:50 -0300

    Add inimigo vermelho e som de tiro

--- Project ARCADE/Assets/Scripts/Enemy2.cs
+++ Project ARCADE/Assets/Scripts/Enemy2.cs
@@ -1,19 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy2 : MonoBehaviour
 {
 public float enemySpeed = 0.2f;
+public GameObject bullet;
+public Transform bulletSpawner;
 Vector3 enemyMoveX = new Vector3 (1,0,0);
 Vector3 enemyMoveZ = new Vector3 (0,0,-0.5f);
-public AudioSource screamSE;
+float nextShot;
+public float fireRate;
 public float enemy1HP = 100;
 public float bulletDamage= 30;
 private void Update() {
     transform.Translate(enemyMoveX * enemySpeed * Time.deltaTime);
     transform.Translate(enemyMoveZ * enemySpeed * Time.deltaTime);
+    ShootPlayer();
 }
+
+    private void ShootPlayer()
+    {
+        if(Time.time > nextShot)
+    {
+        nextShot = Time.time + fireRate;
+            GameObject temporaryBall;
+            temporaryBall = Instantiate(bullet, bulletSpawner.transform.position, bullet.transform.rotation) as GameObject;
+            Rigidbody temporaryBallRB;
+            temporaryBallRB = temporaryBall.GetComponent<Rigidbody>();
+            temporaryBallRB.AddForce(bulletSpawner.transform.forward);   
+            Destroy(temporaryBall, 5f);
+    }
+    }
 
 private void OnCollisionEnter(Collision other) {
     GetHitted();
@@ -21,7 +40,6 @@ private void OnCollisionEnter(Collision other) {
 void GetHitted()
 {
     enemy1HP = enemy1HP - Bullet.bullet1Damage;
-    screamSE.GetComponent<AudioSource>().Play();
     if(enemy1HP <= 0)
     {
     StartCoroutine(Die());
@@ -42,4 +60,5 @@ private void OnTriggerEnter(Collider other) {
 }
 
 }
+
 
---
+++ Project ARCADE/Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+
+    public Vector3 direction = new Vector3 (-1, 0, 0);
+    public float speed = 8;
+    static public float bullet1Damage = 30;
+
+    public Vector3 velocity;
+
+    void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+    void Update()
+    {
+        velocity = direction * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 pos = transform.position;
+
+        pos += velocity * Time.deltaTime;
+
+        transform.position = pos;
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        Destroy(gameObject);
+    }
+}
