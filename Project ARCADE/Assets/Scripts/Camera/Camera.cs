b1cd932b3ea09aeb1b334dfd88a10d36e53a5ca8
lawcows <129429387+lawcows@users.noreply.github.com>
2023-04-11 00:01:34 -0300

    Revert "Camera"
    This reverts commit 0dcf0dee445abd7f17b1cadcd6da61c0300eb4e7, reversing
    changes made to 31782caf3a192ec65b1901260c6f6d361863d339.

--- Project ARCADE/Assets/Scripts/Camera/Camera.cs
+++ Project ARCADE/Assets/Scripts/Camera/Camera.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class Camera : MonoBehaviour
 {
     [SerializeField] Transform cameraGO;
     public float cameraSpeed;
     void Start()
     {
-
+        
     }
 
     void Update()
     {
-        cameraGO.position = new Vector3 (cameraGO.position.x + cameraSpeed*0.01f, cameraGO.transform.position.y, cameraGO.transform.position.z);
+        cameraGO.position = new Vector3 (cameraGO.position.x, cameraGO.transform.position.y, cameraGO.transform.position.z + cameraSpeed*0.01f);
     }
 }
