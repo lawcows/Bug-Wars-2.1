9f23b812d8f4b419675979259320f98fb7d47505
lawcows <129429387+lawcows@users.noreply.github.com>
2023-04-12 17:57:25 -0300

    Add Partycle System
    Implementado Particle system no Enemy1

--- Project ARCADE/Assets/Scripts/Enemies/Enemy.cs
+++ Project ARCADE/Assets/Scripts/Enemies/Enemy.cs
@@ -4,42 +4,57 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-public float enemySpeed = 0.2f;
-Vector3 enemyMoveX = new Vector3 (1,0,0);
-Vector3 enemyMoveZ = new Vector3 (0,0,-0.5f);
-public AudioSource screamSE;
-public float enemy1HP = 100;
-public float bulletDamage= 30;
-private void Update() {
-    transform.Translate(enemyMoveX * enemySpeed * Time.deltaTime);
-    transform.Translate(enemyMoveZ * enemySpeed * Time.deltaTime);
-}
 
-private void OnCollisionEnter(Collision other) {
-    GetHitted();
-}
-void GetHitted()
-{
-    enemy1HP = enemy1HP - Bullet.bullet1Damage;
-    screamSE.GetComponent<AudioSource>().Play();
-    if(enemy1HP <= 0)
-    {
-    StartCoroutine(Die());
+    Vector3 enemyMoveX = new Vector3 (1,0,0);
+    Vector3 enemyMoveZ = new Vector3 (0,0,-0.5f);
+
+
+    public ParticleSystem explosionPS;
+    public AudioSource screamSE;
+
+
+    public float enemySpeed;
+    public float enemy1HP;
+    public float bulletDamage;
+
+
+    private void Update() {
+        transform.Translate(enemyMoveX * enemySpeed * Time.deltaTime);
+        transform.Translate(enemyMoveZ * enemySpeed * Time.deltaTime);
     }
-IEnumerator Die()
-{
-    yield return  new WaitForSeconds(2);
-    Destroy(gameObject);
 
-}
-}
-private void OnTriggerEnter(Collider other) {
-    if(other.tag == "checkpoint")
+    private void OnCollisionEnter(Collision other) {
+        GetHitted();
+    }
+    void GetHitted()
     {
+        enemy1HP = enemy1HP - Bullet.bullet1Damage;
+        screamSE.GetComponent<AudioSource>().Play();
+        ParticleSystem tExplosion = Instantiate(explosionPS, transform.position, Quaternion.identity);
+        StartCoroutine(DestroyPS());
+        if(enemy1HP <= 0)
+        {
+        StartCoroutine(Die());
+        }
+    IEnumerator Die()
+    {
+        yield return  new WaitForSeconds(2);
+        Destroy(gameObject);
+
+    }
+        IEnumerator DestroyPS()
+        {
+            yield return new WaitForSecondsRealtime(1);
+            Destroy(tExplosion);
+        }
+    }
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "checkpoint")
+        {
         
-        enemyMoveX = -enemyMoveX;
+            enemyMoveX = -enemyMoveX;
+        }
     }
-}
 
 }
 
