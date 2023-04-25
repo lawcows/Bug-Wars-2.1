8f3df16b8d7671a09f4dee479857627073352ac7
lawcows <129429387+lawcows@users.noreply.github.com>
2023-04-25 18:37:15 -0300

    Add wing animation + fix models

--- Project ARCADE/Assets/Scripts/Player/Wings.cs
+++ Project ARCADE/Assets/Scripts/Player/Wings.cs
@@ -1,23 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Wings : MonoBehaviour
 {
-    public float wingSpeed;
-    public float wingRotation;
+    public float rotationAngle = 45f; // Angle to rotate by in degrees
+    public float rotationRate = 1f; // Rotation rate in degrees per second
 
-    void Start()
-    {
+    private float currentRotationAngle; // Current angle of rotation
 
-    }
-
-    // Update is called once per frame
     void Update()
-    {        
-        wingRotation = Mathf.PingPong(Time.time * wingSpeed*1000, 90);
-        transform.rotation = Quaternion.AngleAxis(wingRotation,Vector3.right);
-    }
+    {
+        // Calculate the new rotation angle based on the rotation rate and the elapsed time since the last frame
+        float deltaAngle = rotationRate * Time.deltaTime;
+        currentRotationAngle += deltaAngle;
 
-    // se o valor da rotação for menor que 45, wingRotation é Vector3.right
+        // If the current rotation angle exceeds the target angle, wrap around to start over
+        if (currentRotationAngle >= rotationAngle)
+        {
+            currentRotationAngle = 0f;
+        }
+
+        // Rotate the object by the current rotation angle around the Y-axis
+        transform.rotation = Quaternion.Euler(0f, currentRotationAngle, 0f);
+    }
 }
