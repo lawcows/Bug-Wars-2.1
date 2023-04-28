b1f9d95d4a15e6c25436df67bae7cbe94399ce8b
lawcows <129429387+lawcows@users.noreply.github.com>
2023-04-28 12:28:13 -0300

    Restrict player to camera view

--- Project ARCADE/Assets/Scripts/Player/Movement.cs
+++ Project ARCADE/Assets/Scripts/Player/Movement.cs
@@ -6,6 +6,7 @@ public class Movement : MonoBehaviour
 {
     float y = 1, x = 1;
     public float speed = 15;
+    public BoxCollider movementBounds;
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Collectible")
@@ -26,5 +27,18 @@ public class Movement : MonoBehaviour
         {
             transform.Translate(x * speed * Time.deltaTime, 0, y * speed * Time.deltaTime);
         }
+
+
+        // Get the current position of the player
+        Vector3 currentPosition = transform.position;
+
+        // Clamp the position to stay inside the movementBounds
+        Vector3 clampedPosition = new Vector3(
+            Mathf.Clamp(currentPosition.x, movementBounds.bounds.min.x, movementBounds.bounds.max.x),
+            Mathf.Clamp(currentPosition.y, movementBounds.bounds.min.y, movementBounds.bounds.max.y),
+            Mathf.Clamp(currentPosition.z, movementBounds.bounds.min.z, movementBounds.bounds.max.z));
+
+        // Update the position of the player
+        transform.position = clampedPosition;
     }
 }
